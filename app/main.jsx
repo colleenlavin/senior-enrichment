f875410e7c965e1331a216aeb90940390df3b015
Dan <daniel.sohval@gmail.com>
2017-05-24 16:16:43 -0400

    dan fixed some stuff

--- app/main.jsx
+++ app/main.jsx
@@ -4,6 +4,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, hashHistory, IndexRedirect, IndexRoute } from 'react-router';
 import store from './store'
+import axios from 'axios'
 import Home from './components/home'
 import Root from './components/Root'
 import CampusesContainer from './containers/CampusesContainer'
@@ -20,7 +21,7 @@ const onAppEnter = () => {
     .all([pCampuses, pStudents])
     .then(responses => responses.map(r => r.data))
     .then(([campuses, students]) => {
-      store.dispatch(receiveCampuses(campuses));
+      store.dispatch(receiveCampuses(campuses)); //Import these actions
       store.dispatch(receiveStudents(students));
     });
 };
@@ -35,20 +36,20 @@ const onStudentEnter = function (nextRouterState) {
   store.dispatch(getStudentById(studentId));
 };
 
-export default function Main() {
+export default function() {
   return (
+    //Rethink your routing config
     <Provider store={store}>
-
+    <Router history={hashHistory}>
       <Route path='/' component={Root} onEnter={onAppEnter} >
         <Route path='/campuses' component={CampusesContainer} />
         <Route path='/campuses/:campusId' component={CampusContainer} onEnter={onCampusEnter} />
         <Route path='/students' component={StudentsContainer} />
         <Route path='/students/:studentId' component={StudentContainer} onEnter={onStudentEnter} />
-        <IndexRedirect to='/home' />
       </Route>
+    </Router>  
 
-    </Provider> ,
-    document.getElementById('main')
+    </Provider> 
   )
 }
 
